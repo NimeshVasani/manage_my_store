6d12024902a16586d444f9039583481f928f1b27
Nimesh Vasani <64627988+NimeshVasani@users.noreply.github.com>
2024-02-23 00:45:47 -0500

    Web  Update

--- lib/web/ui/sliderscreens/dashboard.dart
+++ lib/web/ui/sliderscreens/dashboard.dart
@@ -1,12 +1,12 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:manage_my_store/web/ui/widgets/dashboard_slider_widgets/charts/income_out_going_chart.dart';
 
 import '../widgets/dashboard_slider_widgets/charts/earning_by_item_type_chart.dart';
 import '../widgets/dashboard_slider_widgets/charts/sales_analysis_chart.dart';
 import '../widgets/dashboard_slider_widgets/charts/top_selling_items_chart.dart';
 import '../widgets/dashboard_slider_widgets/charts/weekly_earning_goals_chart.dart';
+import '../widgets/dashboard_slider_widgets/other_widgets/visit_widget.dart';
 
 class DashBoard extends StatefulWidget {
   const DashBoard({super.key});
@@ -20,12 +20,14 @@ class _DashBoardState extends State<DashBoard> {
   late int flex2;
   late double height1;
   late double height2;
+  double width = PlatformDispatcher.instance.displays.first.size.width;
 
   @override
   void initState() {
     super.initState();
     getWidth();
   }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -61,7 +63,7 @@ class _DashBoardState extends State<DashBoard> {
                         child: Container(
                             decoration:
                                 const BoxDecoration(color: Colors.white),
-                            width: PlatformDispatcher.instance.displays.first.size.width / 4 ,
+                            width: width / 4,
                             height: height2,
                             child: const EarningByItemTypesChart()),
                       ),
@@ -70,34 +72,20 @@ class _DashBoardState extends State<DashBoard> {
                   Expanded(
                     child: Column(
                       children: [
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(left: 5.0, bottom: 5.0),
-                          child: Card(
-                            margin: EdgeInsets.zero,
-                            elevation: 5.0,
-                            child: Container(
-                                decoration:
-                                    const BoxDecoration(color: Colors.white),
-                                width:
-                                PlatformDispatcher.instance.displays.first.size.width / 4 - 5,
-                                height: height2/2-5,
-                                child: const IncomeOutgoingsChart()),
-                          ),
+                        DailyVisits(
+                          width: width,
+                          height: height2,
+                          data: "\$12,990",
+                          type: "VISIT",
+                          img: "assets/web/chart1_img.png",
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 5.0, top: 5.0),
-                          child: Card(
-                            elevation: 5.0,
-                            margin: EdgeInsets.zero,
-                            child: Container(
-                                decoration:
-                                    const BoxDecoration(color: Colors.white),
-                                width:
-                                PlatformDispatcher.instance.displays.first.size.width / 4 - 5,
-                                height: height2/2-5,
-                                child: const IncomeOutgoingsChart()),
-                          ),
+                        const SizedBox(height: 10,),
+                        DailyVisits(
+                          width: width,
+                          height: height2,
+                          data: "46.7%",
+                          type: "BOUNCE RATE",
+                          img: "assets/web/chart2_img.png",
                         ),
                       ],
                     ),
@@ -113,50 +101,50 @@ class _DashBoardState extends State<DashBoard> {
         Expanded(
           flex: flex2,
           child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Card(
-                  elevation: 5.0,
-                  child: Container(
-                      decoration: const BoxDecoration(color: Colors.white),
-                      height: height1/2,
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TopSellingItemsDonutChart(),
-                      )),
-                ),
-                const SizedBox(height: 10,),
-                Card(
-                  elevation: 5.0,
-                  child: Container(
-                      decoration: const BoxDecoration(color: Colors.white),
-                      height: height1/2,
-                      child:  const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: WeeklyGoalsChart(),
-                      )),
-                ),
-              ],
-            ),
-
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Card(
+                elevation: 5.0,
+                child: Container(
+                    decoration: const BoxDecoration(color: Colors.white),
+                    height: height1 / 2,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: TopSellingItemsDonutChart(),
+                    )),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Card(
+                elevation: 5.0,
+                child: Container(
+                    decoration: const BoxDecoration(color: Colors.white),
+                    height: height1 / 2,
+                    child: const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: WeeklyGoalsChart(),
+                    )),
+              ),
+            ],
+          ),
         )
       ],
     );
   }
 
-  void getWidth(){
+  void getWidth() {
     var currentWidth = PlatformDispatcher.instance.displays.first.size.width;
-    if(currentWidth<1400){
+    if (currentWidth < 1400) {
       flex1 = 11;
-      flex2= 7;
+      flex2 = 7;
       height1 = 400;
-      height2= 160;
-    }
-    else{
+      height2 = 160;
+    } else {
       flex1 = 11;
-      flex2= 5;
+      flex2 = 5;
       height1 = 450;
       height2 = 210;
     }
--- lib/web/ui/widgets/dashboard_slider_widgets/other_widgets/visit_widget.dart
+++ lib/web/ui/widgets/dashboard_slider_widgets/other_widgets/visit_widget.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:manage_my_store/mobile/ui/widgets/customtext.dart';
 
 class DailyVisits extends StatefulWidget {
-  const DailyVisits({super.key});
+  final double height;
+  final double width;
+  final String data;
+  final String type;
+  final String img;
+  const DailyVisits({super.key, required this.height, required this.data, required this.width, required this.img, required this.type});
 
   @override
   State<DailyVisits> createState() => _DailyVisitsState();
@@ -10,6 +17,37 @@ class DailyVisits extends StatefulWidget {
 class _DailyVisitsState extends State<DailyVisits> {
   @override
   Widget build(BuildContext context) {
-    return
+    return  Padding(
+      padding: const EdgeInsets.only(left: 5.0),
+      child: Card(
+          elevation: 5.0,
+          margin: EdgeInsets.zero,
+          child: Container(
+              decoration: const BoxDecoration(color: Colors.white),
+              width: widget.width/4 -5,
+              height: widget.height/2 -5,
+              child:  Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          customTextView(widget.data, 20, FontWeight.w600, Colors.black.withOpacity(0.75), 1),
+                          customTextView(widget.type, 14, FontWeight.w600, Colors.black45, 1)
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 30.0),
+                      child: Image.asset(widget.img,fit: BoxFit.cover,height: 50,width: 100,),
+                    )
+                  ],
+                ),
+              )
+        ),
+    );
   }
 }
